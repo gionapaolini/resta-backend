8e5da7a24ffb8cf55bb0fae852868a6b6c9557d9
Giona Paolini <gionapaolini@gmail.com>
2022-03-27 17:09:03 +0200

    Add fake test Entity for testing purposes

---
+++ pkg/eventutils/fakes.go
@@ -0,0 +1,80 @@
+package eventutils
+
+import (
+	"encoding/json"
+
+	"github.com/Resta-Inc/resta/pkg/utils"
+)
+
+type TestEntity struct {
+	*Entity
+	State TestState
+}
+
+type TestState struct {
+	Name string
+}
+
+func NewTestEntity() TestEntity {
+	var event IEvent
+	entityID := utils.GenerateNewUUID()
+
+	testEntity := TestEntity{
+		Entity: &Entity{},
+	}
+
+	event = TestEntityCreated{
+		EntityEventInfo: NewEntityEventInfo(entityID),
+		Name:            "TestEvent",
+	}
+	return AddNewEvent(testEntity, event).(TestEntity)
+}
+
+func (testEntity TestEntity) ChangeName(name string) TestEntity {
+	newEvent := TestEntityNameChanged{
+		EntityEventInfo: NewEntityEventInfo(testEntity.ID),
+		NewName:         name,
+	}
+	testEntity = AddNewEvent(testEntity, newEvent).(TestEntity)
+
+	return testEntity
+}
+
+type TestEntityCreated struct {
+	EntityEventInfo
+	Name string
+}
+
+func (event TestEntityCreated) Apply(entity IEntity) IEntity {
+	testEntity := entity.(TestEntity)
+	testEntity.State.Name = event.Name
+	testEntity.ID = event.EntityID
+	return testEntity
+}
+
+type TestEntityNameChanged struct {
+	EntityEventInfo
+	NewName string
+}
+
+func (event TestEntityNameChanged) Apply(entity IEntity) IEntity {
+	testEntity := entity.(TestEntity)
+	testEntity.State.Name = event.NewName
+	return testEntity
+}
+
+func (testEntity TestEntity) DeserializeEvent(jsonData []byte) IEvent {
+	eventType, rawData := GetRawDataFromSerializedEvent(jsonData)
+	switch eventType {
+	case "TestEntityCreated":
+		var e TestEntityCreated
+		json.Unmarshal(rawData, &e)
+		return e
+	case "TestEntityNameChanged":
+		var e TestEntityNameChanged
+		json.Unmarshal(rawData, &e)
+		return e
+	default:
+		return nil
+	}
+}
