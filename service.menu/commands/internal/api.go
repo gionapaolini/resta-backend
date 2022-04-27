152ab12b71090dabe7058c24a48d795ba2cde01b
Giona Paolini <gionapaolini@gmail.com>
2022-04-27 22:42:25 +0200

    add api call to enable menu

--- service.menu/commands/internal/api.go
+++ service.menu/commands/internal/api.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Resta-Inc/resta/menu/commands/internal/entities"
 	"github.com/Resta-Inc/resta/pkg/eventutils"
+	"github.com/gofrs/uuid"
 	"github.com/gorilla/mux"
 )
 
@@ -21,10 +22,39 @@ func SetupApi(router *mux.Router, repo eventutils.IEntityRepository) {
 
 func (api Api) setupRoutes(r *mux.Router) {
 	r.HandleFunc("/menus", api.CreateNewMenu).Methods("POST")
+	r.HandleFunc("/menus/{id}/enable", api.EnableMenu).Methods("POST")
 }
 
 func (api Api) CreateNewMenu(w http.ResponseWriter, r *http.Request) {
 	menu := entities.NewMenu()
-	api.repository.SaveEntity(menu)
+	err := api.repository.SaveEntity(menu)
+	if err != nil {
+		//FIX IT with not found as well
+		http.Error(w, "something wrong", http.StatusInternalServerError)
+		return
+	}
 	w.WriteHeader(http.StatusCreated)
+}
+
+func (api Api) EnableMenu(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := uuid.FromStringOrNil(vars["id"])
+	if id == uuid.Nil {
+		http.Error(w, "invalid menu id", http.StatusBadRequest)
+		return
+	}
+	menu, err := api.repository.GetEntity(entities.EmptyMenu(), id)
+	if err != nil {
+		//FIX IT with not found as well
+		http.Error(w, "something wrong", http.StatusInternalServerError)
+		return
+	}
+	menu = menu.(entities.Menu).Enable()
+	api.repository.SaveEntity(menu)
+	if err != nil {
+		//FIX IT with not found as well
+		http.Error(w, "something wrong", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
--- service.menu/commands/internal/api_test.go
+++ service.menu/commands/internal/api_test.go
@@ -1,10 +1,12 @@
 package internal
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/Resta-Inc/resta/menu/commands/internal/entities"
 	"github.com/Resta-Inc/resta/pkg/eventutils"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/mock"
@@ -30,4 +32,35 @@ func TestCreateNewMenu(t *testing.T) {
 
 	// Assert
 	require.Equal(t, http.StatusCreated, recorder.Code)
+}
+
+func TestEnableMenu(t *testing.T) {
+	// Arrange
+	menu := entities.NewMenu()
+	mockEntityRepository := new(eventutils.MockEntityRepository)
+	mockEntityRepository.
+		On("GetEntity", entities.EmptyMenu(), menu.ID).
+		Return(menu, nil)
+
+	mockEntityRepository.
+		On("SaveEntity", mock.MatchedBy(
+			func(menu entities.Menu) bool {
+				return menu.IsEnabled()
+			},
+		)).
+		Return(nil)
+	router := mux.NewRouter()
+	SetupApi(router, mockEntityRepository)
+	recorder := httptest.NewRecorder()
+
+	url := fmt.Sprintf("/menus/%s/enable", menu.ID)
+	request, err := http.NewRequest(http.MethodPost, url, nil)
+	require.NoError(t, err)
+
+	// Act
+	router.ServeHTTP(recorder, request)
+
+	// Assert
+	require.Equal(t, http.StatusOK, recorder.Code)
+	mockEntityRepository.AssertExpectations(t)
 }
