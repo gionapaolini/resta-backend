1c41d671c3684abfc5689f13247ca4e1b6c03bf1
Giona Paolini <gionapaolini@gmail.com>
2022-12-24 13:00:46 +0100

    remove cache from tests

--- dev/run_all_gotests.sh
+++ dev/run_all_gotests.sh
@@ -4,4 +4,4 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 ROOT=$SCRIPTPATH/..
 cd $ROOT
 
-find -name go.mod -printf "%h\n" | sort -nr | while read -r i; do cd "$i"; go test -cover ./...; cd $ROOT; done
+find -name go.mod -printf "%h\n" | sort -nr | while read -r i; do cd "$i"; go test -count=1 -cover ./...; cd $ROOT; done
